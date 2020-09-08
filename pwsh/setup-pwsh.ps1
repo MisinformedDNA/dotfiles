4fc9290cea2d3db2b94e25e4ed1e342ae32f0fa2
MisinformedDNA <1784452+MisinformedDNA@users.noreply.github.com>
2020-09-08 15:30:35 -0500

    Install modules

--- pwsh/setup-pwsh.ps1
+++ pwsh/setup-pwsh.ps1
@@ -1,5 +1,8 @@
 Write-Output "Setting up Powershell..."
 
+Install-Module posh-git
+Install-Module ZLocation  
+
 $source = Join-Path $PSScriptRoot *
 $destDir = Split-Path -Parent $PROFILE
 Copy-Item $source $destDir -Recurse -ErrorAction SilentlyContinue
