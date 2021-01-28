9a29f36ee9859615f4c7bf8a84a5406121042213
MisinformedDNA <1784452+MisinformedDNA@users.noreply.github.com>
2021-01-27 19:09:44 -0600

    Add boxstarter script

---
+++ boxstarter.ps1
@@ -0,0 +1,27 @@
+
+# http://boxstarter.org/package/nr/url?https://raw.githubusercontent.com/MisinformedDNA/dotfiles/master/boxstarter.ps1
+
+# Runtimes
+#==========
+
+choco install git --params "/NoShellIntegration /NoGitLfs"
+
+
+choco install visualstudio2019enterprise --params "--add Microsoft.VisualStudio.Workload.Azure --add Microsoft.VisualStudio.Workload.NetCoreTools
+--add Microsoft.VisualStudio.Workload.VisualStudioExtension"
+
+# choco install notepadplusplus
+
+
+# cinst powertoys
+
+# cinst microsoft-edge
+# cinst treesizefree
+
+# cinst vscode
+# cinst vscode-csharp
+# cinst vscode-powershell
+# cinst vscode-icons
+# cinst vscode-gitlens
+# cinst vscode-codespellchecker
+# cinst vscode-markdownlint
