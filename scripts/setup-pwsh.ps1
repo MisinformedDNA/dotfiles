9364e552867410055ed603714dacff854898128a
MisinformedDNA <1784452+MisinformedDNA@users.noreply.github.com>
2021-02-05 13:59:41 -0600

    Fix paths

--- scripts/setup-pwsh.ps1
+++ scripts/setup-pwsh.ps1
@@ -3,6 +3,6 @@ Write-Output "Setting up Powershell..."
 Install-Module posh-git -Force
 Install-Module ZLocation -Force
 
-$source = Join-Path $PSScriptRoot *
+$source = Join-Path $PSScriptRoot "../pwsh"
 $destDir = Split-Path -Parent $PROFILE
 Copy-Item $source $destDir -Recurse -ErrorAction SilentlyContinue
