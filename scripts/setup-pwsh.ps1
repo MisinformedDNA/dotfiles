13f5d2ea89a6ee50ed1875dd4e0004cc7d56dc1a
MisinformedDNA <1784452+MisinformedDNA@users.noreply.github.com>
2021-02-11 16:09:06 -0600

    Cleanup

--- scripts/setup-pwsh.ps1
+++ scripts/setup-pwsh.ps1
@@ -5,5 +5,4 @@ Install-Module ZLocation -Force
 
 $source = Join-Path $PSScriptRoot "../pwsh/*"
 $destDir = Split-Path -Parent $PROFILE
-Write-Output "Copying files from $source to $destDir"
-Copy-Item $source $destDir -Recurse
+Copy-Item $source $destDir -Recurse -ErrorAction SilentlyContinue
