e6845d255cb1b1d1251d096ced015f8b57ac1151
Dan Friedman <dan@thefriedmans.org>
2023-09-05 14:56:15 -0500

    Customize Windows further (#9)

--- scripts/Initialize-Windows.ps1
+++ scripts/Initialize-Windows.ps1
@@ -1,11 +1,28 @@
-# Show hidden files, Show protected OS files, Show file extensions
+# Shows hidden files, protected OS files, and file extensions
 Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions
 
-# will expand explorer to the actual folder you're in
+# Expands the navigation pane to the current folder in File Explorer
 Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneExpandToCurrentFolder -Value 1
-#adds things back in your left pane like recycle bin
+
+# Shows all folders in the navigation pane of File Explorer (like Recycle Bin)
 Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneShowAllFolders -Value 1
-#opens PC to This PC, not quick access
+
+# Changes the default view of File Explorer
+#   This PC: 1
+#   Quick Access: 2
 Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name LaunchTo -Value 1
-#taskbar where window is open and on main taskbar for multi-monitor
+
+# Changes where taskbar buttons are displayed
+#   All taskbars: 0
+#   Main taskbar and taskbar where window is open: 1
+#   Taskbar where window is open: 2
 Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name MMTaskbarMode -Value 1
+
+# Hides the "Task View" button on the taskbar
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name ShowTaskViewButton -Type DWord -Value 0
+
+# Hides the "News and Interests" button on the taskbar
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Feeds -Name ShellFeedsTaskbarViewMode -Type DWord -Value 2
+
+# Hides the search box on the taskbar
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search -Name SearchBoxTaskbarMode -Value 0 -Type DWord -Force
