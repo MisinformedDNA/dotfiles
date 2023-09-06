133052977f21ac77266f408616c2900bc84ab3c3
Dan Friedman <dan@thefriedmans.org>
2023-09-06 10:12:42 -0500

    Only add LOL gitconfig if it doesn't already exist

--- scripts/Install-Apps.ps1
+++ scripts/Install-Apps.ps1
@@ -25,10 +25,12 @@ git config --global alias.cp cherry-pick
 # Set LOL git config
 if ($env:UserDomain -eq "ENT") {
     $customConfigPath = Join-Path $env:UserProfile ".lol.gitconfig"
-    New-Item $customConfigPath -ItemType File -ErrorAction SilentlyContinue
-    git config --file=$customConfigPath user.email "[Email]"
+    if (-not (Test-Path $customConfigPath)) {
+        New-Item $customConfigPath -ItemType File -ErrorAction SilentlyContinue
+        git config --file=$customConfigPath user.email "[Email]"
 
-    git config --global includeIf.gitdir:C:/repos/lol/.path $customConfigPath
+        git config --global includeIf.gitdir:C:/repos/lol/.path $customConfigPath
+    }
 }
 
 choco upgrade tortoisegit
