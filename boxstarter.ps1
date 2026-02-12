897cc9484eab9e30f8cadcab202b34029cadd036
MisinformedDNA <1784452+MisinformedDNA@users.noreply.github.com>
2026-02-11 20:09:03 -0600

    Ensure Git is installed

--- boxstarter.ps1
+++ boxstarter.ps1
@@ -2,17 +2,22 @@
 http://boxstarter.org/package/nr/url?https://raw.githubusercontent.com/MisinformedDNA/dotfiles/main/boxstarter.ps1
 #>
 
+# Ensure git is installed
+if (-not (Get-Command git -ErrorAction SilentlyContinue)) {
+    Write-Host "Installing git..."
+    winget install --id Git.Git -e --silent
+    refreshenv
+}
+
 # # Clone dotfiles
 Write-Host "Clone dotfiles"
 $reposPath = "/repos"
 $dotfilesPath = Join-Path $reposPath dotfiles
-New-Item $reposPath -ItemType Directory
+New-Item $reposPath -ItemType Directory -Force | Out-Null
 Set-Location $reposPath
 git clone https://github.com/MisinformedDNA/dotfiles/
 
 Set-Location $dotfilesPath
-git checkout main
-git pull
 
 . (Join-Path $dotfilesPath scripts/Initialize-Windows.ps1)
 . (Join-Path $dotfilesPath scripts/Install-Apps.ps1)
